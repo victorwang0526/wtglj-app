0e9f975c70d149204407e91b8cfcb98d00c1e812
Nemo Leung <destinylsy520@gmail.com>
2020-08-29 23:28:08 +0800

    增加参数

--- src/pages/enterprise-list/enterprise-list.ts
+++ src/pages/enterprise-list/enterprise-list.ts
@@ -1,18 +1,17 @@
 import { Component } from '@angular/core';
-import {ActionSheetController, AlertController, NavController, NavParams} from 'ionic-angular';
-import {TaskProvider} from "../../providers/task-provider";
-import {UserVo} from "../../models/user-vo";
-import {Storage} from "@ionic/storage";
-import {IndustryEnterpriseVo} from "../../models/industry-enterprise-vo";
-import {CallNumber} from "@ionic-native/call-number";
-import {EnterpriseVo} from "../../models/enterprise-vo";
+import { ActionSheetController, AlertController, NavController, NavParams } from 'ionic-angular';
+import { TaskProvider } from '../../providers/task-provider';
+import { UserVo } from '../../models/user-vo';
+import { Storage } from '@ionic/storage';
+import { IndustryEnterpriseVo } from '../../models/industry-enterprise-vo';
+import { CallNumber } from '@ionic-native/call-number';
+import { EnterpriseVo } from '../../models/enterprise-vo';
 
 @Component({
   selector: 'page-enterprise-list',
   templateUrl: 'enterprise-list.html',
 })
 export class EnterpriseListPage {
-
   key: string = '';
   loading: boolean = false;
   user: UserVo = null;
@@ -21,37 +20,45 @@ export class EnterpriseListPage {
   industries: Array<string> = new Array<string>();
   selectedIndustry: string = '';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-              public taskProvider: TaskProvider,
-              private callNumber: CallNumber,
-              public alertController: AlertController,
-              public actionSheetCtrl: ActionSheetController,
-              public storage: Storage) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public taskProvider: TaskProvider,
+    private callNumber: CallNumber,
+    public alertController: AlertController,
+    public actionSheetCtrl: ActionSheetController,
+    public storage: Storage,
+  ) {
     storage.get('user').then((u: UserVo) => {
       this.user = u;
-      if(u.deptDTO == null) {
+      if (u.deptDTO == null) {
         alertController.create({
           title: '用户未关联部门，请联系管理员',
-          buttons: [{
-            text: '确定',
-            handler: () => {
-              navCtrl.pop({});
-            }
-          }]
+          buttons: [
+            {
+              text: '确定',
+              handler: () => {
+                navCtrl.pop({});
+              },
+            },
+          ],
         });
         return;
       }
       this.loading = true;
-      taskProvider.getIndustryEnterprise(u.deptDTO.areas).subscribe(data => {
-        this.industryEnterprises = data;
-        for(let ie of data) {
-          this.industries.push(ie.industryName);
-        }
-      }, error => {},
+      taskProvider.getIndustryEnterprise(u.deptDTO.areas, this.user.id).subscribe(
+        (data) => {
+          this.industryEnterprises = data;
+          for (let ie of data) {
+            this.industries.push(ie.industryName);
+          }
+        },
+        (error) => {},
         () => {
-        this.loading = false;
-        })
-    })
+          this.loading = false;
+        },
+      );
+    });
   }
 
   openEnterprise(enterprise: EnterpriseVo) {
@@ -62,19 +69,19 @@ export class EnterpriseListPage {
           text: '拨打：' + enterprise.contact + '  ' + enterprise.mobile,
           handler: () => {
             this.call(enterprise);
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
     actionSheet.present();
   }
 
   call(enterprise: EnterpriseVo) {
-    this.callNumber.callNumber(enterprise.mobile, true)
-      .then(res => console.log('Launched dialer!', res))
-      .catch(err => console.log('Error launching dialer', err));
+    this.callNumber
+      .callNumber(enterprise.mobile, true)
+      .then((res) => console.log('Launched dialer!', res))
+      .catch((err) => console.log('Error launching dialer', err));
   }
-
 
   keyChange(e: any) {
     this.key = e.target.value;
@@ -83,5 +90,4 @@ export class EnterpriseListPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad EnterpriseListPage');
   }
-
 }
